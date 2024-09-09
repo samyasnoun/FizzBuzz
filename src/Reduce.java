76c95175d928cf95fcfefb99e5797d30bd8d816b
samyasnoun <158087041+samyasnoun@users.noreply.github.com>
2024-09-09 19:04:47 -0400

    hello

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,5 +1,18 @@
 public class Reduce {
     public static void main(String[] args) {
+        int n = 100;
+        int x = 0;
+        while (n != 0){
+            if (n % 2 == 0) {
+                n /= 2;
+            }else if (n % 2 == 1){
+                n = n - 1;
+                }
+            x++;
+
+            }
+        System.out.println(x);
+        }
 
     }
-}
+
